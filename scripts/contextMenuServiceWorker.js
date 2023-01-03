2f7311a6e0e5a3c4e34b5346b5fd7fa39c45a3d5
GOATsann <ikechukwuobiokoye2@gmail.com>
2023-01-03 18:25:33 -0500

    -m

--- scripts/contextMenuServiceWorker.js
+++ scripts/contextMenuServiceWorker.js
@@ -1,4 +1,49 @@
-const generateCompletionAction = async (info) => {}
+const getKey = () => {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get(['openai-key'], (result) => {
+      if (result['openai-key']) {
+        const decodedKey = atob(result['openai-key']);
+        resolve(decodedKey);
+      }
+    });
+  });
+};
+const generate = async (prompt) => {
+  // Get your API key from storage
+  const key = await getKey();
+  const url = 'https://api.openai.com/v1/completions';
+	
+  // Call completions endpoint
+  const completionResponse = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${key}`,
+    },
+    body: JSON.stringify({
+      model: 'text-davinci-003',
+      prompt: prompt,
+      max_tokens: 30,
+      temperature: 0.5,
+    }),
+  });
+	
+  // Select the top choice and send back
+  const completion = await completionResponse.json();
+  return completion.choices.pop();
+}
+
+const generateCompletionAction = async (info) => {
+  try {
+    const { selectionText } = info;
+    const basePromptPrefix = "convert a "
+    const basePromptPrefix2 = " emoji to a text drawing with at least 10 characters. Do not give me a result that has less than 10 characters. for example, a smiley face would be: {{=^__^=}}";
+  const baseCompletion = await generate(`${basePromptPrefix}${selectionText}${basePromptPrefix2}`);
+  console.log(baseCompletion.text)
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
