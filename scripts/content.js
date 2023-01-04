51c06d6b295c07116fd81994fdc5952898055aed
GOATsann <ikechukwuobiokoye2@gmail.com>
2023-01-04 12:15:22 -0500

    -m

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,6 @@
 const insert = (content) => {
 // Find Calmly editor input section
-const elements = document.getElementsByClassName('droid');
+const elements = document.getElementsByClassName('add deez');
 
 if (elements.length === 0) {
   return;
